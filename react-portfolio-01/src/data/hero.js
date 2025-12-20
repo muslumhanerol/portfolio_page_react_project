dbe208f6ff895c180841cf83194235ae414a0759
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-12-20 23:30:58 +0300

    Update hero.js

---
+++ react-portfolio-01/src/data/hero.js
@@ -0,0 +1,15 @@
+export const heroData = {
+    greeting: "Hello, I'm",
+    name: "Muslum Han Erol",
+    role: [
+        "Frontend Developer",
+        "UI/UX Designer",
+        "React Specialist",
+        "Creative Coder"
+
+    ],
+    description: "I am a passionate frontend developer with a knack for creating engaging user experiences. I specialize in React and have a strong background in UI/UX design.",
+    state: [
+        { number: "5+", label: "Years Experience" }
+    ]
+}
