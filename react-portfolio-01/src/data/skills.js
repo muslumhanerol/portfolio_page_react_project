24855e876c79d8beb4862893e46a8097a196e739
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-12-24 23:14:28 +0300

    Create skills.js

---
+++ react-portfolio-01/src/data/skills.js
@@ -0,0 +1,5 @@
+export const skills = [
+    { category: "Frontend", skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Bootstrap"] },
+    { category: "Backend", skills: ["Node.js", "Express", "MongoDB", "SQL", "RESTful APIs"] },
+    { category: "Tools & Platforms", skills: ["Git", "GitHub", "Docker", "AWS", "Jest"] }
+];
