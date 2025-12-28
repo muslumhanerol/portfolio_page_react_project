6a18cd7aee1492938d2969abd3ec637ca670ab65
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-12-28 23:57:20 +0300

    use_scroll

--- react-portfolio-01/src/hooks/useScrollToTop.js
+++ react-portfolio-01/src/hooks/useScrollToTop.js
@@ -1,1 +1,23 @@
 import { useState, useEffect } from "react";
+
+export const useScrollToTop = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollPosition(window.scrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (scrollPosition > 100) {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        }
+    }, [scrollPosition]);
+};
