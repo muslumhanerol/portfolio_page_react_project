fa75203c108face39d51f9d7cdc3db0c88b3a5e2
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-01-02 23:17:06 +0300

    Update Navigation.jsx

--- react-portfolio-01/src/components/Navigation.jsx
+++ react-portfolio-01/src/components/Navigation.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
 
 const Navigation = () => {
+    const [IsMobilMenuOpen, setIsMobilMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
 }
 
