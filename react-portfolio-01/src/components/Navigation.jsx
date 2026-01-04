bc095e9914b372b9c97d706196068da49e953e0b
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-01-04 23:49:58 +0300

    Update Navigation.jsx

--- react-portfolio-01/src/components/Navigation.jsx
+++ react-portfolio-01/src/components/Navigation.jsx
@@ -28,11 +28,12 @@ const Navigation = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(window.scrollY > 0);
+            setIsScrolled(window.scrollY > 50);
         };
 
-    }
-    )
+        window.addEventListener("scroll", handleScroll);
+
+    })
 
 }
 
