cf155ee4bef1b0ca1fa4146b92397cdfa4c993f5
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-01-04 23:56:24 +0300

    Update Navigation.jsx

--- react-portfolio-01/src/components/Navigation.jsx
+++ react-portfolio-01/src/components/Navigation.jsx
@@ -40,8 +40,11 @@ const Navigation = () => {
     const navItems = [
         { href: "#about", label: "About" },
         { href: "#projects", label: "Projects" },
+        { href: "#skills", label: "Skills" },
         { href: "#contact", label: "Contact" },
-    ]
+    ];
+
+
 }
 
 export default Navigation;
