37e0214d31bb293b5f8659b3c4f4043cd0c1ddfa
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-01-18 23:50:37 +0300

    Update Hero.js

--- react-portfolio-01/src/components/Hero.js
+++ react-portfolio-01/src/components/Hero.js
@@ -1,3 +1,3 @@
 import React, { useState, useEffect } from 'react';
 import { AArrowDown, Github, Linkedin, Mail } from 'lucide-react';
-import herodata from '../data/herodata';
+import { herodata } from '../data/herodata';
