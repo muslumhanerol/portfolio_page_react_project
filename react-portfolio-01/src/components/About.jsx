186dc5e9532eb432d076458fdddb8bb0bcd4ec57
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-02-25 22:25:36 +0300

    Update About.jsx

--- react-portfolio-01/src/components/About.jsx
+++ react-portfolio-01/src/components/About.jsx
@@ -1,2 +1,8 @@
 import React from "react";
 import { Github, Linkedin, Mail, Code, Paletto, Smartphone, ExternalLink } from "lucide-react";
+
+const About = ({ hasAnimated }) => {
+
+}
+
+export default About;
