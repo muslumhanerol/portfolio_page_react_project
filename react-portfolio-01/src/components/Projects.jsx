7f0e1480a159821dfb306cc0d1af6389c1b1a042
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-03-05 23:57:26 +0300

    Update Projects.jsx

--- react-portfolio-01/src/components/Projects.jsx
+++ react-portfolio-01/src/components/Projects.jsx
@@ -3,7 +3,13 @@ import { projects } from "../data/projects";
 
 const Projects = ({ hasAnimated }) => {
     return (
-        <section id="projects" className={`py-16 px-6 bg-white`}></section>
+        <section id="projects" className={`py-16 px-6`}>
+            <div className="max-w-6xl mx-auto">
+                <div className={`transition-all duration-1000 delay-300 ${hasAnimated.projects ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>
+                    <h2></h2>
+                </div>
+            </div>
+        </section>
     )
 
 }
