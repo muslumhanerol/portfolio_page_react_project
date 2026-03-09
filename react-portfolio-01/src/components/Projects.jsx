38497eec50fb4fce563cf651576f13a6a87a8956
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-03-09 23:58:09 +0300

    Update Projects.jsx

--- react-portfolio-01/src/components/Projects.jsx
+++ react-portfolio-01/src/components/Projects.jsx
@@ -9,7 +9,9 @@ const Projects = ({ hasAnimated }) => {
                 <div className={`transition-all duration-1000 delay-300 ${hasAnimated.projects ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>
                     <h2 className="text-4xl font-bold mb-12 text-center">Featured Projects</h2>
                     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-9">
-                        {projects.map((project, index) => (ProjectCard))}
+                        {projects.map((project, index) => (<ProjectCard key={index}
+                            project={project} />
+                        ))}
                     </div>
                 </div>
             </div>
