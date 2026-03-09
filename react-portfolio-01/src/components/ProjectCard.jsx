86b2d8d79ad7f8d5943b5849c9955d76de9856d2
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2026-03-10 00:04:00 +0300

    Update ProjectCard.jsx

--- react-portfolio-01/src/components/ProjectCard.jsx
+++ react-portfolio-01/src/components/ProjectCard.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Github, ExternalLink } from "lucide-react";
 
 const ProjectCard = ({ project }) => {
+    return (
+        <div className="group bg-white border border-gray-200 rounded-lg overflow-hidden hover:shadow-lg transition-all duration-300 hover:translate-y-1 flex flex-col h-full">
+            <div className="h-48 bg-gradient-to-br  from-gray-100 to-gray-200 flex-items-center justify-content"></div>
+        </div>
+    )
 
 }
 
