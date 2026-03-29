7a55f7d713ea13ee753608f71e43e01ecd8b506d
muslumhanerol <mslmhanerol@gmail.com>
2026-03-29 23:21:46 +0300

    Update Skills.jsx

--- react-portfolio-01/src/components/Skills.jsx
+++ react-portfolio-01/src/components/Skills.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { skills } from '../data/skills';
 
 const Skills = ({ hasAnimated }) => {
+    return (
+        <section id="skills" className="py-16 px-6 bg-gray-50"></section>
+    )
 
 }
 
