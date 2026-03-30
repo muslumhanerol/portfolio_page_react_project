0e19c61c388d99ef33f8d250901cbb2c5cf77ef3
muslumhanerol <mslmhanerol@gmail.com>
2026-03-30 23:52:48 +0300

    Update Skills.jsx

--- react-portfolio-01/src/components/Skills.jsx
+++ react-portfolio-01/src/components/Skills.jsx
@@ -9,6 +9,11 @@ const Skills = ({ hasAnimated }) => {
                     <h2 className="text-4xl font-bold mb-12 text-center">
                         Skills & Technologies 
                     </h2>
+                    <div className="grid md:grid-cols-3 gap-8">
+                        {skills.map((skillGroup,index)=>
+                        
+                        )}
+                    </div>
                 </div>
             </div>
         </section>
