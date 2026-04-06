ceb7bd12407d711dd226ced93840a4bb2e4fccec
muslumhanerol <mslmhanerol@gmail.com>
2026-04-06 23:58:06 +0300

    contatact_label

--- react-portfolio-01/src/App.jsx
+++ react-portfolio-01/src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
 import Skills from "./components/Skills.jsx";
+import Contact from "./components/Contact.jsx";
 
 const App = () => {
   const hasAnimated = useIntersectionObserver();
@@ -18,6 +19,7 @@ const App = () => {
       <About hasAnimated={hasAnimated} />
       <Projects hasAnimated={hasAnimated} />
       <Skills hasAnimated={hasAnimated} />
+      <Contact hasAnimated={hasAnimated} />
     </div>
   );
 };
--- react-portfolio-01/src/components/Contact.jsx
+++ react-portfolio-01/src/components/Contact.jsx
@@ -1,3 +1,14 @@
 import React from "react";
 import {Mail, Github, Linkedin} from "lucide-react";
 import { contactLinks } from '../data/contact';
+
+const Contact = ({ hasAnimated }) => {
+    const contactLinksWithIcons = contactLinks.map
+    (link => ({
+        ...link,
+        icon: link.label === "Email Me" ? <Mail className="w-5 h-5 mr-2" /> : link.label === "Github" ? <Github className= "w-5 h-5 mr-2" /> 
+    }))
+
+}
+
+export default Contact;
