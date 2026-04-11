1bf3088fe100253f4f4654b4431614fa0426168e
muslumhanerol <mslmhanerol@gmail.com>
2026-04-12 00:23:00 +0300

    rebuild

--- react-portfolio-01/src/App.jsx
+++ react-portfolio-01/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Navigation from "./components/Navigation";
-import { useIntersectionObserver } from "./hooks/useIntersectionObserver.js";
+import { useIntersectionObserver } from "./hooks/uselntersectionObserver.js";
 import { useScrollToTop } from "./hooks/useScrollToTop";
-import Hero from "./components/Hero";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
 import Skills from "./components/Skills.jsx";
 import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
   const hasAnimated = useIntersectionObserver();
@@ -16,7 +16,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-white text-black">
-      <Navigation />
+      <Navigation />      
       <Hero hasAnimated={hasAnimated} />
       <About hasAnimated={hasAnimated} />
       <Projects hasAnimated={hasAnimated} />
